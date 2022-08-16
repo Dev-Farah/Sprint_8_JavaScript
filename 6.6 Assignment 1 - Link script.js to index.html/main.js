1017c724dacccd4f6d265d86e180862f36650847
FarahSyed <farahsyed44@gmail.com>
2022-08-16 09:45:15 +0500

    Add lesson 6 assignments

---
+++ 6.6 Assignment 1 - Link script.js to index.html/main.js
@@ -0,0 +1,2 @@
+// Console.log an updated greeting from a second, main.js file, using a “src” source attribute.
+console.log("Hello World! from main.js");
---
+++ 6.8 Assignment 3 - Use single-line and multi-line commenting/app.js
@@ -0,0 +1,20 @@
+// Assignment 6.8: Use single-line and multi-line commenting.
+
+// Describe the new section of our code, turn off and on lines of code, and leave a note to yourself for later - all using the comment syntax.
+
+// Tasks:
+//     List your name, your birthday, the name of your favorite pet, and the city you would love to travel to next.
+//     Give it “a heading” “Personal Info” as a comment.
+//     Practice commenting-out lines of code by “turning off” your new third line - the one with your pet’s name. Then, re-run your file to confirm that the line no longer runs!
+//     Turn off this entire section of code:
+//         First, un-comment the line with your pet’s information (so that it’s turned back on) .
+//         Second, use multi-line commenting to turn off all four new lines.
+//     Add an earlier note to yourself that is adjacent to your code, using in-line commenting.
+
+// Solution:
+
+// Personal Info
+/* console.log("My name is Farah Syed"); // Inline Comment
+console.log("My birthday is on January 1, 1990");
+console.log("My favorite pet is Tommy");
+console.log("I would love to travel Seoul next"); */
