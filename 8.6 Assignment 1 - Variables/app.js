f117777bf5b8f9d8b4eacb733755dc0f4eb3a082
FarahSyed <farahsyed44@gmail.com>
2022-08-18 07:55:35 +0500

    Add lesson 8 assignment 1

---
+++ 8.6 Assignment 1 - Variables/app.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// Declare a variable.
+// Declare a variable using var keyword
+var userName;
+console.log(userName);
+
+// Declare a variable using let keyword
+let age;
+console.log(age);
+
+// Declare a variable using const keyword
+const position;
+console.log(position);
+
+
+
+// Initialize your variables
+// Initialize your variable using var keyword
+userName = "Ali";
+console.log(userName);
+
+// Initialize your variable using let keyword
+age = 25;
+console.log(age);
+
+// Initialize your variable using const keyword
+position = "Manager";
+console.log(position);
+
+
+
+// Redefine your variables
+// Redefine your variable using var keyword
+userName = "Hina";
+console.log(userName);
+
+// Redefine your variable using let keyword
+age = 30;
+console.log(age);
+
+// Redefine your variable using const keyword
+position = "Sales Manager";
+console.log(position);
+
+
+// Conclusion:
+// var:
+// A variable with keyword "var" can be declared again and redefined
+
+// let:
+// A variable with keyword "let" cannot be declared again with the same name but it can be redefined
+
+// const:
+// A variable with keyword "const" cannot be declared again or redefined
