cba53131928e9c5c89aa0e215edeba0585c4c5b0
FarahSyed <farahsyed44@gmail.com>
2022-08-20 15:21:33 +0500

    Add lesson 9 assignment

---
+++ 9.6 Assignment 1 - Equals/app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// In this assignment you will learn to check elements for:
+
+// Equivalence (value),
+// Strict equivalence (value and type),
+// Non-equivalency
+
+// For both a string and a number, and two strings
+
+// Tasks:
+// Test equivalence (type)
+console.log(3 == "3");
+
+// Test strict equivalence (value and type)
+console.log(3 === "3");
+
+// Test not equivalent and strict not equivalent
+console.log(3 != "3");
+console.log(3 !== "3");
+
+// Use equivalency to test whether (or not) one string is greater than another
+console.log("m" > "a");
